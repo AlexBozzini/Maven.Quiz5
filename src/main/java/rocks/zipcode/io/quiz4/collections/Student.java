b17920e6b09af308dd894e0b964ca648c8e6425f
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-08 14:07:18 -0400

    Finished collections except simple string iterator

--- src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/Student.java
@@ -4,17 +4,33 @@ package rocks.zipcode.io.quiz4.collections;
  * @author leon on 11/12/2018.
  */
 public class Student {
+    private Integer id;
+    private Double numberOfHours;
+
     public Student() {
         this(null);
+        this.numberOfHours = 0.0;
     }
 
     public Student(Integer id) {
+        this.id = id;
+        this.numberOfHours = 0.0;
+    }
+
+    public Student(Integer id, Double numberOfHours){
+        this.id = id;
+        this.numberOfHours = numberOfHours;
     }
 
     public void learn(Double amountOfHours) {
+        this.numberOfHours += amountOfHours;
     }
 
     public Double getTotalStudyTime() {
-        return null;
+        return this.numberOfHours;
+    }
+
+    public void setTotalStudyTime(Double numberOfHours){
+        this.numberOfHours = numberOfHours;
     }
 }
--- src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
+++ src/main/java/rocks/zipcode/io/quiz4/collections/ZipCodeWilmington.java
@@ -1,22 +1,43 @@
 package rocks.zipcode.io.quiz4.collections;
 
-import java.util.Map;
+import java.util.*;
 
 /**
  * @author leon on 11/12/2018.
  */
 public class ZipCodeWilmington {
+    private List<Student> students;
+
+    public ZipCodeWilmington(List<Student> students){
+        this.students = students;
+    }
+
+    public ZipCodeWilmington(){
+        this.students = new ArrayList<>();
+    }
+
     public void enroll(Student student) {
+        students.add(student);
     }
 
     public Boolean isEnrolled(Student student) {
-        return null;
+        if (students.contains(student)){
+            return true;
+        }
+        return false;
     }
 
-    public void lecture(double numberOfHours) {
+    public void lecture(Double numberOfHours) {
+        for (Student s : students){
+            s.setTotalStudyTime(s.getTotalStudyTime() + numberOfHours);
+        }
     }
 
     public Map<Student, Double> getStudyMap() {
-        return null;
+        Map<Student, Double> studyMap = new LinkedHashMap<>();
+        for (Student s : students){
+            studyMap.put(s, s.getTotalStudyTime());
+        }
+        return studyMap;
     }
 }
