c6c379f2bb63d212b6745b321e01e2e2d2fbac94
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-08 16:53:40 -0400

    First attempt group

--- src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/Group.java
@@ -1,37 +1,65 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import com.sun.tools.javac.util.GraphUtils;
+
 import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
 
 /**
  * @author leon on 18/12/2018.
  */
-public class Group<_> {
+public class Group<_> implements Iterable<_>{
+    private List<_> items;
+
     public Group() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        this.items = new LinkedList<>();
+    }
+
+    public Group(List<_> items) {
+        this.items = items;
     }
 
     public Integer count() {
-        return null;
+        return items.size();
     }
 
     public void insert(_ value) {
+        items.add(value);
     }
 
     public Boolean has(_ value) {
-        return null;
+        return items.contains(value);
     }
 
     public _ fetch(int indexOfValue) {
-        return null;
+        return items.get(indexOfValue);
     }
 
     public void delete(_ value) {
+        items.remove(value);
     }
 
     public void clear() {
+        items.clear();
     }
 
+    public String toString(){
+        return items.toString();
+    }
+
+    @Override
     public Iterator<_> iterator() {
-        return null;
+        return new Iterator<_>() {
+            @Override
+            public boolean hasNext() {
+                return false;
+            }
+
+            @Override
+            public _ next() {
+                return null;
+            }
+        };
     }
 }
