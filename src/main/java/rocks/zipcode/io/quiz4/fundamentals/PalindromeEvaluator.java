063dacdf323fee4ca9aa869576877bdc2451e5ac
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-08 11:29:57 -0400

    Finished palindrome evaluator

--- src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
+++ src/main/java/rocks/zipcode/io/quiz4/fundamentals/PalindromeEvaluator.java
@@ -1,18 +1,41 @@
 package rocks.zipcode.io.quiz4.fundamentals;
 
+import java.util.*;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class PalindromeEvaluator {
     public static String[] getAllPalindromes(String string) {
-        return null;
+        String[] allSubstrings = StringEvaluator.getAllSubstrings(string);
+        Set<String> palindromeSet = new HashSet<>();
+        for (String s : allSubstrings){
+            if (isPalindrome(s)){
+                palindromeSet.add(s);
+            }
+        }
+        String[] palindromeArr = new String[palindromeSet.size()];
+        Integer count = 0;
+        for (String s : palindromeSet){
+            palindromeArr[count] = s;
+            count++;
+        }
+        return palindromeArr;
     }
 
     public static Boolean isPalindrome(String string) {
-        return null;
+        if (string.equals(reverseString(string))){
+            return true;
+        }
+        return false;
     }
 
     public static String reverseString(String string) {
-        return null;
+        StringBuilder builder = new StringBuilder();
+        char[] chars = string.toCharArray();
+        for (int i = chars.length - 1; i >= 0; i--){
+            builder.append(chars[i]);
+        }
+        return builder.toString();
     }
 }
