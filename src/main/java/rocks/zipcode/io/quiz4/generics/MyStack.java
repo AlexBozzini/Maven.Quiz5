593402fcd9a02d4c55e2673ce772e84095882048
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-08 14:16:46 -0400

    Finished myStack

--- src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/MyStack.java
@@ -1,25 +1,53 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import sun.invoke.empty.Empty;
+
+import java.util.ArrayList;
+import java.util.EmptyStackException;
+import java.util.Iterator;
+import java.util.List;
+
 /**
  * @author leon on 11/12/2018.
  */
-public class MyStack<SomeType> {
+public class MyStack<SomeType> implements Iterable<SomeType> {
+    private List<SomeType> items;
+
     public MyStack() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        this.items = new ArrayList<>();
+    }
+
+    public MyStack(List<SomeType> items){
+        this.items = items;
     }
 
     public Boolean isEmpty() {
-        return null;
+        return items.isEmpty();
     }
 
     public void push(SomeType i) {
+        items.remove(i);
+        items.add(items.size(), i);
     }
 
     public SomeType peek() {
-        throw new UnsupportedOperationException("Method not yet implemented");
+        if (!isEmpty()) {
+            return items.get(items.size() - 1);
+        }
+        return null;
     }
 
     public SomeType pop() {
+        if (!isEmpty()) {
+            SomeType item = peek();
+            items.remove(items.size() - 1);
+            return item;
+        }
+        throw new EmptyStackException();
+    }
+
+    @Override
+    public Iterator<SomeType> iterator() {
         return null;
     }
 }
