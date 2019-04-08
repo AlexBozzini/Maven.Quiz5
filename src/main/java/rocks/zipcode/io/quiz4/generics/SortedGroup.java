9c0dd5804d0e59712173d5521d565b39c5f671a0
Alexandra Bozzini <alexandrabozzini@zipcoders-MacBook-Pro-74.local>
2019-04-08 17:13:00 -0400

    Attempted sorted group

--- src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
+++ src/main/java/rocks/zipcode/io/quiz4/generics/SortedGroup.java
@@ -1,18 +1,35 @@
 package rocks.zipcode.io.quiz4.generics;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author leon on 18/12/2018.
  */
 public class SortedGroup<_> extends Group<_> {
+    List<_> items;
+
+    public SortedGroup(){
+        this.items = new ArrayList<>();
+    }
+
+    public SortedGroup(List<_> items){
+        this.items = items;
+    }
+
     @Override
     public void insert(_ value) {
+        for (int i = 0; i < items.size(); i++){
+
+        }
     }
 
     @Override
     public void delete(_ value) {
+        items.remove(value);
     }
 
     public Integer indexOf(_ value) {
-        return null;
+        return items.indexOf(value);
     }
 }
